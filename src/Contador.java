bcf181ff0184bfe4eea99ddab118b4c1b4a29730
Solmayer Alves <solmayer.alves@gmail.com>
2024-09-10 20:51:25 -0300

    feat: criando projeto de contagem entre dois números

---
+++ src/Contador.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+            int num1, num2;
+
+        System.out.println("Digite o primeiro número:");
+            num1 = sc.nextInt();
+
+        System.out.println("Digite o segundo número:");
+            num2 = sc.nextInt();
+
+        sc.close();
+
+    }
+}
